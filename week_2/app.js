32881e3c039b02e740e259faafab1e363854b91a
nikolalsvk <nikolaseap@gmail.com>
2016-09-22 16:56:23 -0500

    Finish week_2 assignment

---
+++ week_2/app.js
@@ -0,0 +1,44 @@
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+  .controller('ToBuyShoppingController',
+              ['ShoppingListCheckOffService', ToBuyShoppingController])
+  .controller('AlreadyBoughtShoppingController',
+              ['ShoppingListCheckOffService', AlreadyBoughtShoppingController])
+
+
+  function ShoppingListCheckOffService() {
+    var service = this
+
+    var toBuy = ["10 cookies", "12 eggs", "13 apples"]
+    var bought = []
+
+    service.buy = function(index, item) {
+      toBuy.splice(index, 1)
+      bought.push(item)
+    }
+
+    service.getToBuy = function() {
+      return toBuy
+    }
+
+    service.getBought = function() {
+      return bought
+    }
+  }
+
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    this.toBuy = ShoppingListCheckOffService.getToBuy();
+
+    this.buy = function(index, item) {
+      ShoppingListCheckOffService.buy(index, item);
+    }
+  }
+
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    this.bought = ShoppingListCheckOffService.getBought();
+  }
+
+})();
