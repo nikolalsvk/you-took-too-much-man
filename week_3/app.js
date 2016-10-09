d1899161bd1696ab169e329f5169d69c3f22f916
nikolalsvk <nikolaseap@gmail.com>
2016-10-09 23:36:58 +0200

    Fetch items from API

---
+++ week_3/app.js
@@ -0,0 +1,59 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .service('MenuSearchService', ['$http', MenuSearchService])
+  .controller('NarrowItDownController', ['MenuSearchService', NarrowItDownController])
+  .directive('foundItems', foundItems)
+
+  function MenuSearchService($http, searchTerm) {
+    var service = this
+
+    service.run = function(searchTerm) {
+      service.searchTerm = searchTerm;
+      service.foundItems = [];
+
+      return $http({
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+      }).then(function(response) {
+        service.foundItems = response.data["menu_items"]
+
+        service.foundItems.forEach(function(item, index) {
+          console.log(searchTerm);
+          console.log(index);
+          var isSubstring = item["description"].indexOf(service.searchTerm) !== -1;
+          console.log(isSubstring);
+
+          if(isSubstring) {
+
+          } else {
+            service.foundItems.splice(index, 1);
+          }
+        })
+
+        return service.foundItems;
+      });
+    }
+  }
+
+  function NarrowItDownController(MenuSearchService) {
+    var narrowDwnCtrl = this;
+    narrowDwnCtrl.MenuSearchService = MenuSearchService;
+
+    narrowDwnCtrl.getMatchedMenuItems = function(searchTerm) {
+     narrowDwnCtrl.found = narrowDwnCtrl.MenuSearchService.run(searchTerm);
+    }
+  }
+
+  function foundItems() {
+    return {
+      restrict: 'A',
+      scope: {
+        found: "&found"
+      },
+      template: "<button ng-repeat='item in found'>Don't want this one</button>"
+    }
+  }
+
+})();
